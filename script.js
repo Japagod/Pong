f5052f23bb93a1224e847e8dd1be8182a872cf76
Japagod <soPessoalBreno@gmail.com>
2021-10-20 17:19:29 +0000

    bar 2

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ var py = 0
 document.addEventListener("keydown",move)
 
 function move(event){
-    var bar = document.getElementById("barra")
+    var bar1 = document.getElementById("barra1")
+    var bar2 = document.getElementById("barra2")
     var tecla = event.keyCode
         if(tecla==38){
             if(py>0){
             py-=10
-            bar.style.top=py+"px"
+            bar1.style.top=py+"px"
         }
         } if(tecla==40){
             if(py<611){
             py+=7
-            bar.style.top=py+"px"
+            bar1.style.top=py+"px"
+            }   
+        }
+        if(tecla==38){
+            if(py>0){
+            py-=10
+            bar2.style.top=py+"px"
+        }
+        } if(tecla==40){
+            if(py<611){
+            py+=7
+            bar2.style.top=py+"px"
             }   
         }
 }
