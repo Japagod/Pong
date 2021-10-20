eb737cc9eb3da3c516e09ce5b60c96fe4334d760
Japagod <soPessoalBreno@gmail.com>
2021-10-20 18:19:45 +0000

    again again

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var keysPressed = {}
 var py1 =  0
 var py2 = 0
 
-document.addEventListener('keydown', (move) => {
-    keysPressed[move.key] = true;
+document.addEventListener('keydown', (event) => {
+    keysPressed[event.key] = true;
  
-    if (keysPressed['Control'] && move.key == 'a') {
-        alert(move.key);
+    if (keysPressed['Control'] && event.key == 'ArrowDown') {
+        alert(event.key);
     }
  });
  
- document.addEventListener('keyup', (move) => {
-    delete keysPressed[move.key];
+ document.addEventListener('keyup', (event) => {
+    delete keysPressed[event.key];
  });
 
 function move(event){
