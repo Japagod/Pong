976145245e5b3432e19bc7d1a3afceeee72da48d
Japagod <soPessoalBreno@gmail.com>
2021-10-20 17:46:33 +0000

    bar keys

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ function move(event){
         if(tecla==38){
             if(py1>0){
             py1-=7
-            bar1.style.top=py1+"px"
+            bar2.style.top=py1+"px"
         }
         } if(tecla==40){
             if(py1<611){
             py1+=7
-            bar1.style.top=py1+"px"
+            bar2.style.top=py1+"px"
             }   
         }
         if(tecla==87){
             if(py2>0){
             py2-=7
-            bar2.style.top=py2+"px"
+            bar1.style.top=py2+"px"
         }
         } if(tecla==83){
             if(py2<611){
             py2+=7
-            bar2.style.top=py2+"px"
+            bar1.style.top=py2+"px"
             }   
         }
 }
